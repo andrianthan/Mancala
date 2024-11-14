21fca9d723fc3241685a1db5abf32dfb19cd2580
Nathan Dinh <nathanxdinhx97@gmail.com>
2024-11-14 12:28:13 -0800

    Initialize Mancala model with board setup and basic game logic

--- src/MancalaModel.java
+++ src/MancalaModel.java
@@ -1,7 +1,42 @@
 import java.util.ArrayList;
 import java.util.Iterator;
-iport java.util.List;
+import java.util.List;
 import java.util.*;
 public class MancalaModel {
+    private int[] pits;
+    private int mancalaA, mancalaB;
+    private int numStonesPerPit;
 
+    public MancalaModel(int numStonesPerPit) {
+        this.numStonesPerPit = numStonesPerPit;
+        pits = new int[12];
+        mancalaA = 0;
+        mancalaB = 0;
+        initializaBoard();
+    }
+
+    private void initializaBoard() {
+        for (int i = 0; i < numStonesPerPit; i++) {
+            pits[i] = numStonesPerPit;
+        }
+    }
+
+    public int getMancalaA() {
+        return mancalaA;
+    }
+
+    public int getMancalaB() {
+        return mancalaB;
+    }
+
+    public void moveStones(int pitIndex, boolean isInTurn) {
+        int stones = pits[pitIndex];
+        pits[pitIndex] = 0;
+        int currentIndex = pitIndex;
+        while (stones > 0) {
+            currentIndex = (currentIndex + 1) % pits.length;
+            pits[currentIndex]++;
+            stones--;
+        }
+    }
 }

164dbfb4e8ffa5f9225b33ac4df04cb96a8b35fb
Karla Nguyen <karlaa09861@gmail.com>
2024-11-14 15:09:54 -0800

    Mancala view template

--- src/MancalaView.java
+++ src/MancalaView.java
@@ -1,2 +1,25 @@
-public class MancalaView {
+import javax.swing.*;
+import java.awt.*;
+
+public class MancalaView extends JFrame {
+    private MancalaModel model;
+    //need to add buttons and labels
+
+    public MancalaView(MancalaModel model) {
+        this.model = model;
+        setupGUI();
+    }
+
+    private void setupGUI() {
+        //set up screen
+        setTitle("Mancala Game");
+        setSize(800, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        //still need to add components for the game board (buttons, pits, mancalas)
+    }
+
+    //update board view when player makes a move
+    public void updateBoard(){
+
+    }
 }
